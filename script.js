19024bbabead0cff04487a9b2ba8d27cdd7ac1b9
tylergraybeal4 <83237773+tylergraybeal4@users.noreply.github.com>
2024-11-14 15:03:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 const images = [
-  'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/image000000.jpg?raw=true',
-  'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/IMG_6912.JPG?raw=true',
-  'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/IMG_5652.JPG?raw=true',
-  'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/IMG_5651.JPG?raw=true',
-  'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/IMG_5650.JPG?raw=true',
-  'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/IMG_5649.JPG?raw=true'
+  'https://iili.io/2Af8hf1.jpg',
+  'https://iili.io/2Af8VWB.jpg',
+  'https://iili.io/2Af81Ox.jpg',
+  'https://iili.io/JGBltp9.jpg',
+  'https://iili.io/JGBlbIe.jpg',
+  'https://iili.io/2Afkerg.jpg',
+  'https://iili.io/2AfkXIV.jpg',
+  'https://iili.io/JGB022V.jpg',
+  'https://iili.io/2AfvpSa.jpg',
+  'https://iili.io/JGBlZk7.jpg',
+  'https://iili.io/2AfkVmQ.jpg',
+  'https://iili.io/JGB0HEx.jpg',
+  'https://iili.io/2Afk6QI.jpg',
+  'https://iili.io/2Af8MxV.jpg',
+  'https://iili.io/2Af8WiP.jpg',
+  'https://iili.io/2AfUqyG.jpg',
+  'https://iili.io/2AfUfvs.jpg',
+  'https://iili.io/2AfUCuf.jpg',
+  'https://iili.io/2AfUIGS.jpg',
+  'https://iili.io/2AfUlZQ.jpg',
+  'https://iili.io/2AfU7yb.jpg',
+  'https://iili.io/2AfU1nV.jpg',
+  'https://iili.io/2AfUG6P.jpg',
+  'https://iili.io/2AfUcwx.jpg',
+  'https://iili.io/2AfUEMB.jpg',
+  'https://iili.io/2AfUVF1.jpg',
+  'https://iili.io/2AfUWcF.jpg',
+  'https://iili.io/2AfUX8g.jpg',
+  'https://iili.io/2Af8OJa.jpg',
+  'https://iili.io/2Afk0ru.jpg',
+  'https://iili.io/2Af87f9.jpg',
+  'https://iili.io/2Af8gsI.jpg',
+  'https://iili.io/2Af860X.jpg',
+  'https://iili.io/2Af84ft.jpg',
+  'https://iili.io/2AfS7Xp.jpg',
+  'https://iili.io/2AfSYLN.jpg',
+  'https://iili.io/2AfS3ej.jpg',
+  'https://iili.io/2AfSMkG.jpg',
+  'https://iili.io/2AfSG7s.jpg',
+  'https://iili.io/2AfSL41.jpg',
+  'https://iili.io/2AfSiCB.jpg',
+  'https://iili.io/2AfSZ3F.jpg',
+  'https://iili.io/2AfSbyJ.jpg',
+  'https://iili.io/2AfSyjR.jpg',
+  'https://iili.io/2AfUdGI.jpg',
+  'https://iili.io/2Afvmcg.jpg',
+  'https://iili.io/2Af8FVI.jpg',
+  'https://iili.io/2Af8KPt.jpg',
+  'https://iili.io/2Af8CSs.jpg',
+  'https://iili.io/2Af8aUu.jpg',
+  'https://iili.io/2AfviwQ.jpg',
+  'https://iili.io/2AfvZMP.jpg',
+  'https://iili.io/2AfkiBt.jpg',
+  'https://iili.io/2AfvCTQ.jpg',
+  'https://iili.io/2AfvoZB.jpg',
+  'https://iili.io/2AfkhhB.jpg',
+  'https://iili.io/2AfkNB1.jpg'
 ];
 
-const extendedImages = [...images, ...images, ...images, ...images];
+
+const extendedImages = [...images];
 
 const gallery = document.getElementById('gallery');
 const overlay = document.getElementById('overlay');
@@ -60,7 +112,7 @@ function handleDownload(e) {
   e.preventDefault();
   
   // Set the URL of the image to download
-  const imageUrl = 'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/IMG_5651.JPG?raw=true';
+  const imageUrl = 'https://github.com/tylergraybeal4/Modeling-Portfolio/blob/main/Modeling-pics/IMG_5652.JPG?raw=true';
 
   // Create an anchor element and set the download attributes
   const link = document.createElement('a');
